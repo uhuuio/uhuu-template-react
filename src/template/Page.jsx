56460aa994b4551bbe318b619a159d6f06d12d5d
Guvener Gokce <guvener@guvenergokce.com>
2022-06-20 13:42:48 +0200

    Update Page.jsx

--- src/template/Page.jsx
+++ src/template/Page.jsx
@@ -7,9 +7,9 @@ export function Page(props) {
 		<div className="text-center font-firamono bg-indigo-100 text-indigo-600 h-screen flex items-center justify-center px-12">
         <div>
           <div className="font-bold text-6xl text-center">
-            Hey,{payload.first_name} {payload.last_name}
+            Hey,{payload?.first_name} {payload?.last_name}
           </div>
-          {payload.message ? (
+          {payload?.message ? (
             <div className="py-6 text-4xl border-4 border-indigo-500 rounded-full bg-white mt-12">
               {payload.message}
             </div>
