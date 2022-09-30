ce230a6fa1208be30824fcf746cda4cb803dc5a2
Guvener Gokce <guvener@guvenergokce.com>
2022-09-30 12:28:01 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,34 +2,40 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import uhuu from 'vite-plugin-uhuu'
 import fs from 'fs';
-
+import path from 'path';
 
 export default ({ mode }) => {
     // Load app-level env vars to node-level env vars.
     process.env = {...process.env, ...loadEnv(mode, process.cwd())};
 
-    // Local development must be over SSL, set up certificate.
+    // Check if there is a certificate.
     const keyPath = process.env.VITE_HTTPS_KEY;
     const certPath = process.env.VITE_HTTPS_CERT;
 
-    // Ensure certifacates are set
-    if(!keyPath || !certPath) {
-      console.log('⛔️ Server should run over https, please set up a certificate. (Readme.md)');
-      process.exit();
-    }
-
-    console.log(mode);
-
-    // vite config
-    return defineConfig({
-      base: '', // deployed uhuu template should have empty base path (relative).
-      server: {
-        https: {
-          key: fs.readFileSync(keyPath),
-          cert: fs.readFileSync(certPath),
-        },
+    let config = {
+      base: '',
+      resolve:{
+        alias:{
+          '@' : path.resolve(__dirname, './src') // alias to src directory
+        }
       },
       plugins: [react(), uhuu()]
-    })
+    };
+
+    // if there is not a certifacate, fallback to http
+    if(!keyPath || !certPath) {
+      console.log('Running server over HTTP, no env key/cert set up.'); // Set up certificates to serve over HTTPS, check readme.md
+      return defineConfig(config);
+    }
+
+    // if there is a certificate, configure
+    config.server = {
+      https: {
+        key: fs.readFileSync(keyPath),
+        cert: fs.readFileSync(certPath),
+      },
+    };
+
+    return defineConfig(config);
 }
 
